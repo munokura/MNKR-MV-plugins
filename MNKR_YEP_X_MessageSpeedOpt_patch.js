521a026d607dbceb25f23c560f6b2500333b2e7e
munokura <munokura@users.noreply.github.com>
2020-12-11 19:48:45 +0900

    Release

---
+++ MNKR_YEP_X_MessageSpeedOpt_patch.js
@@ -0,0 +1,22 @@
+/*
+ * --------------------------------------------------
+ * MNKR_YEP_X_MessageSpeedOpt_patch Ver.1.0.0
+ * Copyright (c) 2020 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
+//============================================================================
+// Utilities
+//============================================================================
+
+Yanfly.Util = Yanfly.Util || {};
+
+if (!Yanfly.Util.toGroup) {
+
+Yanfly.Util.toGroup = function(inVal) {
+  return inVal;
+}
+
+};
