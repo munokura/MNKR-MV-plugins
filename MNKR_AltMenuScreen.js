264b00850542ca262d0fa98619335c24f4fad2e3
munokura <36838778+munokura@users.noreply.github.com>
2024-09-22 09:11:54 +0900

    ヘルプ修正

--- MNKR_AltMenuScreen.js
+++ MNKR_AltMenuScreen.js
@@ -2,92 +2,91 @@
 // AltMenuScreen.js
 //============================================================================
 
-/*:ja
+/*:
  * @target MV
  * @url https://raw.githubusercontent.com/munokura/MNKR-MV-plugins/master/MNKR_AltMenuScreen.js
  * @plugindesc メニュー画面のレイアウトを変更します。
- * @author Yoji Ojima (改変 munokura)
+ * @author Yoji Ojima (改変 ムノクラ)
  *
- * @help このプラグインには、プラグインコマンドはありません。
+ * @help
+ * # 機能
+ * RPGツクールMV公式プラグイン(AltMenuScreen.js)を改変したものです。
+ * メインメニュー画面をシンプルにしましたす。
  *
- * AltMenuScreenプラグインを改変し、
- * メインメニュー画面をシンプルにしたものです。
+ * このプラグインには、プラグインコマンドはありません。
+ *
+ * # 注意事項
+ * 質問は改変者へお願いいたします。
  *
  *
- *
- *   RPGツクールMV公式プラグインを改変したものです。
- *   質問は改変者へお願いいたします。
- *
- *
- * 利用規約:
- *   ツクールシリーズ利用規約のみ守ってください。
- *   https://tkool.jp/support/index.html
- *   RPGツクールMVのユーザ登録が必要です。
- *   作者に無断で改変、再配布が可能で、
- *   利用形態（商用、18禁利用等）についても制限はありません。
+ * # 利用規約:
+ * - GGG製品利用規約を守ってください。
+ * それ以外の制限はありません。
+ * 作者に無断で改変、再配布が可能です。
+ * 利用形態（商用、18禁利用等）についても制限はありません。
  */
 
-(function() {
+(function () {
 
-Window_Base.prototype.drawActorLevel = function(actor, x, y, width) {
-    width = width || 168;
-    this.changeTextColor(this.systemColor());
-    this.drawText(TextManager.levelA, x, y, 48);
-    this.resetTextColor();
-    this.drawText(actor.level, x, y, width, 'right');
-};
+    Window_Base.prototype.drawActorLevel = function (actor, x, y, width) {
+        width = width || 168;
+        this.changeTextColor(this.systemColor());
+        this.drawText(TextManager.levelA, x, y, 48);
+        this.resetTextColor();
+        this.drawText(actor.level, x, y, width, 'right');
+    };
 
     var _Scene_Menu_create = Scene_Menu.prototype.create;
-    Scene_Menu.prototype.create = function() {
+    Scene_Menu.prototype.create = function () {
         _Scene_Menu_create.call(this);
         this._statusWindow.x = 0;
         this._statusWindow.y = this._commandWindow.height + 232;	//munokura
         this._goldWindow.x = Graphics.boxWidth - this._goldWindow.width;
     };
 
-    Window_MenuCommand.prototype.windowWidth = function() {
-        return Graphics.boxWidth/2;	//munokura
+    Window_MenuCommand.prototype.windowWidth = function () {
+        return Graphics.boxWidth / 2;	//munokura
     };
 
-    Window_MenuCommand.prototype.maxCols = function() {
+    Window_MenuCommand.prototype.maxCols = function () {
         return 2;	//munokura
     };
 
-    Window_MenuCommand.prototype.numVisibleRows = function() {
+    Window_MenuCommand.prototype.numVisibleRows = function () {
         return 4;	//munokura
     };
 
-    Window_MenuStatus.prototype.windowWidth = function() {
+    Window_MenuStatus.prototype.windowWidth = function () {
         return Graphics.boxWidth;
     };
 
-    Window_MenuStatus.prototype.windowHeight = function() {
+    Window_MenuStatus.prototype.windowHeight = function () {
         var h1 = this.fittingHeight(1);
         var h2 = this.fittingHeight(2);
         return Graphics.boxHeight - h1 - h2 - 232;	//munokura
     };
 
-    Window_MenuStatus.prototype.maxCols = function() {
-//        return 4;
+    Window_MenuStatus.prototype.maxCols = function () {
+        //        return 4;
         return $gameParty.members().length;	//munokura
     };
 
-    Window_MenuStatus.prototype.numVisibleRows = function() {
+    Window_MenuStatus.prototype.numVisibleRows = function () {
         return 1;
     };
 
-    Window_MenuStatus.prototype.drawItemImage = function(index) {
+    Window_MenuStatus.prototype.drawItemImage = function (index) {
         var actor = $gameParty.members()[index];
         var rect = this.itemRectForText(index);
         var w = Math.min(rect.width, 144);
         var h = Math.min(rect.height, 144);
         var lineHeight = this.lineHeight();
         this.changePaintOpacity(actor.isBattleMember());
-//        this.drawActorFace(actor, rect.x, rect.y + lineHeight * 2.5, w, h);	//munokura
+        //        this.drawActorFace(actor, rect.x, rect.y + lineHeight * 2.5, w, h);	//munokura
         this.changePaintOpacity(true);
     };
 
-    Window_MenuStatus.prototype.drawItemStatus = function(index) {
+    Window_MenuStatus.prototype.drawItemStatus = function (index) {
         var actor = $gameParty.members()[index];
         var rect = this.itemRectForText(index);
         var x = rect.x;
@@ -97,14 +96,14 @@ Window_Base.prototype.drawActorLevel = function(actor, x, y, width) {
         var lineHeight = this.lineHeight();
         this.drawActorName(actor, x, bottom - lineHeight * 5, width);	//munokura
         this.drawActorLevel(actor, x, bottom - lineHeight * 4, width);	//munokura
-//        this.drawActorClass(actor, x, bottom - lineHeight * 4, width);	//munokura
+        //        this.drawActorClass(actor, x, bottom - lineHeight * 4, width);	//munokura
         this.drawActorHp(actor, x, bottom - lineHeight * 3, width);
         this.drawActorMp(actor, x, bottom - lineHeight * 2, width);
         this.drawActorIcons(actor, x, bottom - lineHeight * 1, width);
     };
 
     var _Window_MenuActor_initialize = Window_MenuActor.prototype.initialize;
-    Window_MenuActor.prototype.initialize = function() {
+    Window_MenuActor.prototype.initialize = function () {
         _Window_MenuActor_initialize.call(this);
         this.y = this.fittingHeight(2);
     };
--- MOGHexpan.js
+++ MOGHexpan.js
@@ -2,7 +2,7 @@
 // MOGHexpan.js
 //============================================================================
 
-/*:ja
+/*:
  * @target MV
  * @url https://raw.githubusercontent.com/munokura/MNKR-MV-plugins/master/MOGHexpan.js
  * @plugindesc ver1.04 MOG_BattleHud(v5.04)拡張
@@ -41,7 +41,7 @@
  * 変数ID20の値と同じIDのアクターの戦闘フェイス画像を戦闘開始時のものに戻す。
  *
  *
- * 利用規約(2020/04/03変更)：
+ * # 利用規約(2020/04/03変更)：
  * この作品は マテリアル・コモンズ・ブルー・ライセンスの下に提供されています。
  * https://ja.materialcommons.org/mtcm-b-summary/
  * クレジット表示：まっつＵＰ (改変munokura)
@@ -54,47 +54,47 @@ var MOGHupdateactorId = 0;
 var MOGHupdateaddnum = 0;
 
 var _MOGH_pluginCommand = Game_Interpreter.prototype.pluginCommand;
-Game_Interpreter.prototype.pluginCommand = function(command, args) {
+Game_Interpreter.prototype.pluginCommand = function (command, args) {
 	_MOGH_pluginCommand.call(this, command, args);
-	if(command === 'MOGH') {
+	if (command === 'MOGH') {
 		var valueid = Number(args[1] || 0);
 		var addnum = Number(args[2] || 0);
 		MOGHupdateactorId = $gameVariables.value(valueid);
-		switch(args[0]) {
+		switch (args[0]) {
 			case 'update':
 				MOGHupdateaddnum = MOGHupdateactorId + addnum;
-			break;
+				break;
 		};
 	};
 };
 
 //一応フレームアニメーションの処理内容はフックで残す。
 var _MOGH_update_face_animation = Battle_Hud.prototype.update_face_animation;
-Battle_Hud.prototype.update_face_animation = function() {
+Battle_Hud.prototype.update_face_animation = function () {
 	if (MOGHupdateaddnum > 0 && MOGHupdateactorId === this._battler._actorId) {
 		this.create_faceMOGH();
 	};
 	_MOGH_update_face_animation.call(this);
 };
 
-Battle_Hud.prototype.create_faceMOGH = function() {
+Battle_Hud.prototype.create_faceMOGH = function () {
 
 	if (String(Moghunter.bhud_face_visible) != "true") {
 		return;
 	};
-	
+
 	this.removeChild(this._face);
-	
+
 	if (!this._battler) {
 		return;
 	};
-	
+
 	this._face = new Sprite(ImageManager.loadBHud("Face_" + MOGHupdateaddnum));
 	this._face.anchor.x = 0.5;
 	this._face.anchor.y = 0.5;
-	this._face_data = [0,0,false,false,false,-1];
+	this._face_data = [0, 0, false, false, false, -1];
 	this._face.ph = 0;
-	this._face.animation = [-1,0,0,0,0,0,0,0,0];
+	this._face.animation = [-1, 0, 0, 0, 0, 0, 0, 0, 0];
 	this._face.breathEffect = this._battler._bhud.faceBreath;
 	this._face.scaleY = 0;
 
@@ -105,7 +105,7 @@ Battle_Hud.prototype.create_faceMOGH = function() {
 		this._face_data[4] = true
 	};
 
-	this._battler._bhud_face_data = [0,0,0,1];
+	this._battler._bhud_face_data = [0, 0, 0, 1];
 	this.addChild(this._face);
 	this.refresh_position();
 	this.base_parameter_clear();
