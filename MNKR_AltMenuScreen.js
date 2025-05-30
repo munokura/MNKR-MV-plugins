942faa19e9a6e447297e5e6fa17e58ca1c8c9eae
munokura <36838778+munokura@users.noreply.github.com>
2025-05-30 10:54:39 +0900

    プラグインパラメーター追加

--- MNKR_AltMenuScreen.js
+++ MNKR_AltMenuScreen.js
@@ -1,5 +1,6 @@
 //============================================================================
 // AltMenuScreen.js
+// Ver 1.1.0
 //============================================================================
 
 /*:
@@ -11,7 +12,7 @@
  * @help
  * # 機能
  * RPGツクールMV公式プラグイン(AltMenuScreen.js)を改変したものです。
- * メインメニュー画面をシンプルにしましたす。
+ * メインメニュー画面をシンプルにします。
  *
  * このプラグインには、プラグインコマンドはありません。
  *
@@ -24,9 +25,22 @@
  * それ以外の制限はありません。
  * 作者に無断で改変、再配布が可能です。
  * 利用形態（商用、18禁利用等）についても制限はありません。
+ * 
+ * @param actorCols
+ * @text アクター表示数
+ * @desc メインメニュー画面のアクター表示ウィンドウの1画面の登録数。0にすると、パーティ人数で自動調整
+ * @type number
+ * @min 0
+ * @default 4
  */
 
-(function () {
+(() => {
+    "use strict";
+    const pluginName = document.currentScript.src.split("/").pop().replace(/\.js$/, "");
+    const parameters = PluginManager.parameters(pluginName);
+
+    const param = {};
+    param.actorCols = Number(parameters['actorCols'] || 0);
 
     Window_Base.prototype.drawActorLevel = function (actor, x, y, width) {
         width = width || 168;
@@ -36,25 +50,25 @@
         this.drawText(actor.level, x, y, width, 'right');
     };
 
-    var _Scene_Menu_create = Scene_Menu.prototype.create;
+    const _Scene_Menu_create = Scene_Menu.prototype.create;
     Scene_Menu.prototype.create = function () {
         _Scene_Menu_create.call(this);
         this._statusWindow.x = 0;
-        this._statusWindow.y = this._commandWindow.height + 232;	//munokura
+        this._statusWindow.y = this._commandWindow.height + 232;
         this._goldWindow.x = Graphics.boxWidth - this._goldWindow.width;
-        this._goldWindow.y = 340;	//munokura
+        this._goldWindow.y = 0;
     };
 
     Window_MenuCommand.prototype.windowWidth = function () {
-        return Graphics.boxWidth / 2;	//munokura
+        return Graphics.boxWidth / 2;
     };
 
     Window_MenuCommand.prototype.maxCols = function () {
-        return 2;	//munokura
+        return 2;
     };
 
     Window_MenuCommand.prototype.numVisibleRows = function () {
-        return 4;	//munokura
+        return 4;
     };
 
     Window_MenuStatus.prototype.windowWidth = function () {
@@ -62,14 +76,15 @@
     };
 
     Window_MenuStatus.prototype.windowHeight = function () {
-        var h1 = this.fittingHeight(1);
-        var h2 = this.fittingHeight(2);
-        return Graphics.boxHeight - h1 - h2 - 232;	//munokura
+        const h1 = this.fittingHeight(1);
+        const h2 = this.fittingHeight(2);
+        return Graphics.boxHeight - h1 - h2 - 232;
     };
 
     Window_MenuStatus.prototype.maxCols = function () {
         //        return 4;
-        return $gameParty.members().length;	//munokura
+        const cols = param.actorCols === 0 ? $gameParty.members().length : param.actorCols;
+        return cols;
     };
 
     Window_MenuStatus.prototype.numVisibleRows = function () {
@@ -77,33 +92,24 @@
     };
 
     Window_MenuStatus.prototype.drawItemImage = function (index) {
-        var actor = $gameParty.members()[index];
-        var rect = this.itemRectForText(index);
-        var w = Math.min(rect.width, 144);
-        var h = Math.min(rect.height, 144);
-        var lineHeight = this.lineHeight();
-        this.changePaintOpacity(actor.isBattleMember());
-        //        this.drawActorFace(actor, rect.x, rect.y + lineHeight * 2.5, w, h);	//munokura
-        this.changePaintOpacity(true);
     };
 
     Window_MenuStatus.prototype.drawItemStatus = function (index) {
-        var actor = $gameParty.members()[index];
-        var rect = this.itemRectForText(index);
-        var x = rect.x;
-        var y = rect.y;
-        var width = rect.width;
-        var bottom = y + rect.height;
-        var lineHeight = this.lineHeight();
-        this.drawActorName(actor, x, bottom - lineHeight * 5, width);	//munokura
-        this.drawActorLevel(actor, x, bottom - lineHeight * 4, width);	//munokura
-        //        this.drawActorClass(actor, x, bottom - lineHeight * 4, width);	//munokura
+        const actor = $gameParty.members()[index];
+        const rect = this.itemRectForText(index);
+        const x = rect.x;
+        const y = rect.y;
+        const width = rect.width;
+        const bottom = y + rect.height;
+        const lineHeight = this.lineHeight();
+        this.drawActorName(actor, x, bottom - lineHeight * 5, width);
+        this.drawActorLevel(actor, x, bottom - lineHeight * 4, width);
         this.drawActorHp(actor, x, bottom - lineHeight * 3, width);
         this.drawActorMp(actor, x, bottom - lineHeight * 2, width);
         this.drawActorIcons(actor, x, bottom - lineHeight * 1, width);
     };
 
-    var _Window_MenuActor_initialize = Window_MenuActor.prototype.initialize;
+    const _Window_MenuActor_initialize = Window_MenuActor.prototype.initialize;
     Window_MenuActor.prototype.initialize = function () {
         _Window_MenuActor_initialize.call(this);
         this.y = this.fittingHeight(2);
