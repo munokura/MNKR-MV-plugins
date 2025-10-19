66bfc6c57fb9b4231877aa2aaab2c7e7addf6d1d
munokura <revi3638+git@gmail.com>
2025-10-20 06:14:58 +0900

    rename to MNKR_MessageAlignCenterMV

--- MNKR_MessageAlignCenterMV.js
+++ MNKR_MessageAlignCenterMV.js
@@ -1,3 +1,13 @@
+/*
+ * --------------------------------------------------
+ * MNKR_MessageAlignCenterMV.js
+ *   Ver.0.1.0
+ * Copyright (c) 2025 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
 /*=============================================================================
  MessageAlignCenter.js
 ----------------------------------------------------------------------------
@@ -21,57 +31,116 @@
  [Twitter]: https://twitter.com/triacontane/
  [GitHub] : https://github.com/triacontane/
 =============================================================================*/
-//  MessageAlignCenterMV.js
-//  1.0.1 2025/10/19 MZ版に近い構造へリファクタリング
-//  1.0.0 2025/10/18 MVへ移植 munokura
-
 /*:
- * @plugindesc メッセージの中央揃えプラグイン
- * @author トリアコンタン,（移植munokura）
- *
- * @param escapeCharacterCenter
- * @text 中央揃えの制御文字
- * @desc メッセージを中央揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
- * @default ac
- *
- * @param escapeCharacterRight
- * @text 右揃えの制御文字
- * @desc メッセージを右揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
- * @default ar
- *
- * @param escapeCharacterVCenter
- * @text 縦中央揃えの制御文字
- * @desc メッセージを縦方向中央揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
- * @default vc
- *
- * @param escapeCharacterVBottom
- * @text 縦下揃えの制御文字
- * @desc メッセージを縦方向下揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
- * @default vb
- *
- * @help MessageAlignCenterMV.js
- *
- * 文章の表示などのメッセージやスキルの説明文などを中央揃え、右揃えにできます。
- * 以下の制御文字を行の先頭に記述すると揃えを変更できます。
- * \ac : 中央揃え
- * \ar : 右揃え
- * \vc : 縦方向の中央揃え
- * \vb : 縦方向の下揃え
- *
- * 後ろに数値を指定すると描画位置が指定したピクセル分、左にずれて描画されます。
- * \ac[20]
- *
- * メッセージのプレビューには反映されません。
- * 制御文字は必ず行の先頭に記述してください。
- * 縦方向揃えは、いち文章につきひとつだけ指定できます。
- * 必ず文章の先頭に記述してください。
- * 右読み言語には現バージョンでは対応していません。
- *
- * 利用規約：
- *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
- *  についても制限はありません。
- *  このプラグインはもうあなたのものです。
- */
+@target MV
+@plugindesc Message Centering Plugin
+@author munokura
+@url https://raw.githubusercontent.com/munokura/MNKR-MV-plugins/master/MNKR_MessageAlignCenterMV.js
+@license MIT License
+
+@help
+MessageAlignCenterMV.js
+
+This allows you to center or right-align messages, skill descriptions, and other text.
+You can change the alignment by placing the following control characters at the beginning of a line.
+\ac: Center alignment
+\ar: Right alignment
+\vc: Vertical center alignment
+\vb: Vertical bottom alignment
+
+Adding a number after the character will shift the drawing position to the left by the specified number of pixels.
+\ac[20]
+
+This will not be Reflectioned in the message preview.
+Control characters must be placed at the beginning of a line.
+Only one vertical alignment can be specified per line.
+Always place them at the beginning of the line.
+Right-to-left languages are not supported in this version.
+
+There are no plugin commands.
+
+# Terms of Use
+MIT License.
+http://opensource.org/licenses/mit-license.php
+Modifications and redistribution are permitted without the author's permission.
+There are no restrictions on usage (commercial, R18, etc.).
+
+@param escapeCharacterCenter
+@text Centering control characters
+@desc This is a control character to center the message. Normally you can use it as is, but if it conflicts with other plugins, change it.
+@default ac
+
+@param escapeCharacterRight
+@text Right-justified control characters
+@desc This is a control character that aligns the message to the right. Normally, you can use it as is, but if it conflicts with other plugins, change it.
+@default ar
+
+@param escapeCharacterVCenter
+@text Vertically centered control characters
+@desc This is a control character that vertically centers the message. Normally, you can use it as is, but if it conflicts with other plugins, change it.
+@default vc
+
+@param escapeCharacterVBottom
+@text Vertically aligned control characters
+@desc This is a control character that vertically aligns the message to the bottom. Normally, you can use it as is, but if it conflicts with other plugins, change it.
+@default vb
+*/
+
+
+/*:ja
+@target MV
+@plugindesc メッセージの中央揃えプラグイン
+@author munokura
+@url https://raw.githubusercontent.com/munokura/MNKR-MV-plugins/master/MNKR_MessageAlignCenterMV.js
+@license MIT License
+
+@help
+MessageAlignCenterMV.js
+
+文章の表示などのメッセージやスキルの説明文などを中央揃え、右揃えにできます。
+以下の制御文字を行の先頭に記述すると揃えを変更できます。
+\ac : 中央揃え
+\ar : 右揃え
+\vc : 縦方向の中央揃え
+\vb : 縦方向の下揃え
+
+後ろに数値を指定すると描画位置が指定したピクセル分、左にずれて描画されます。
+\ac[20]
+
+メッセージのプレビューには反映されません。
+制御文字は必ず行の先頭に記述してください。
+縦方向揃えは、いち文章につきひとつだけ指定できます。
+必ず文章の先頭に記述してください。
+右読み言語には現バージョンでは対応していません。
+
+プラグインコマンドはありません。
+
+# 利用規約
+MITライセンスです。
+http://opensource.org/licenses/mit-license.php
+作者に無断で改変、再配布が可能で、
+利用形態（商用、18禁利用等）についても制限はありません。
+
+@param escapeCharacterCenter
+@text 中央揃えの制御文字
+@desc メッセージを中央揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
+@default ac
+
+@param escapeCharacterRight
+@text 右揃えの制御文字
+@desc メッセージを右揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
+@default ar
+
+@param escapeCharacterVCenter
+@text 縦中央揃えの制御文字
+@desc メッセージを縦方向中央揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
+@default vc
+
+@param escapeCharacterVBottom
+@text 縦下揃えの制御文字
+@desc メッセージを縦方向下揃えにする制御文字です。通常はそのままで使えますが、他プラグインと競合する場合は変更してください。
+@default vb
+*/
 
 (() => {
     'use strict';
@@ -272,7 +341,7 @@
     // startX、startY、drawingの初期化
     const _Window_Message_startMessage = Window_Message.prototype.startMessage;
     Window_Message.prototype.startMessage = function () {
-        _Window_Message_startMessage.apply(this, arguments);
+        _Window_Message_startMessage.call(this);
         this._textState.startX = this._textState.x;
         this._textState.startY = this._textState.y;
         this._textState.drawing = true;
